1dbc9beda3bf7dca1c253153c63c58dbb1e615ea
Alberto Cruz <albertocruzmendoza@outlook.com>
2020-05-03 22:47:10 -0700

    Crear pagina de mi perfil

--- src/app/tabs/profile/profile.page.ts
+++ src/app/tabs/profile/profile.page.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { SettingsPage } from '../modals/settings/settings.page';
+import { UserService } from 'src/app/services/user.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { User } from 'src/models/user.model';
+import { Observable } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-profile',
@@ -9,10 +14,25 @@ import { SettingsPage } from '../modals/settings/settings.page';
 })
 export class ProfilePage implements OnInit {
 
-  constructor(public modalCtrl: ModalController) { }
+  user: User;
+
+  constructor(public modalCtrl: ModalController,
+              private userService: UserService,
+              public fAuth: AngularFireAuth) {
+              const userlog = this.fAuth.auth.currentUser;
+              console.log('entro a prfie');
+
+
+    }
+
 
   ngOnInit() {
+    const sessionUser = JSON.parse(localStorage.getItem('user'));
+    console.log(sessionUser.uid); // esto es lo que se necesita para sacar el usuario de firebase
+    this.userService.getUser(sessionUser.uid);
+    console.log(this.user.email);
   }
+
 
   async showSettings() {
     const modal = await this.modalCtrl.create({
@@ -20,5 +40,12 @@ export class ProfilePage implements OnInit {
     });
     return await modal.present();
   }
+
+  // getUser() {
+  //   return this.userService;
+  //   console.log(this.userService);
+  // }
+
+
 
 }
