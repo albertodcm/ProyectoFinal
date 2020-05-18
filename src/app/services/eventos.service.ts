86276381aa55dac75f8d3bcd43b74823f4679bb1
ludamuac <luisa@cetys.edu.mx>
2020-05-18 01:00:58 -0700

    crear servicio eventos y modelo de evento

---
+++ src/app/services/eventos.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Evento } from 'src/models/evento.model';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { OrgPage } from '../tabs/org/org.page';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventosService {
+
+  constructor(private afs: AngularFirestore) { }
+
+  getEvento(eventoId: string) {
+    return this.afs.doc(`eventos/${eventoId}`).snapshotChanges().pipe(
+      map(a => {
+        const data = a.payload.data() as any;
+        const id = a.payload;
+
+        return { id, ...data} as Evento;
+      })
+    );
+  }
+
+  getEventos(): Observable<Evento[]> {
+    return this.afs.collection('eventos').snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as any;
+        const id = a.payload.doc.id;
+
+        return { id, ...data } as Evento;
+      }))
+    );
+  }
+
+  searchEvento(name: string) {
+    return this.afs.collection('eventos', ref => ref
+      .where('nombre', '>=', name)
+      .where('nombre', '<=', name + '\uf8ff')
+      .limit(10)
+      .orderBy('nombre'))
+      .snapshotChanges()
+      .pipe(map(actions => actions.map(a => {
+        return a.payload.doc.data();
+      }))
+    );
+  }
+
+  // solo para crear los dummies, esto va en la app de empresas
+  createEvento(evento: Evento): Promise<void> {
+    evento.id = this.afs.createId();
+
+    return this.afs.doc(`eventos/${evento.id}`).set(evento);
+  }
+
+
+}
---
+++ src/models/evento.model.ts
@@ -0,0 +1,10 @@
+export interface Evento {
+    id: string;
+    nombre: string;
+    fecha: string;
+    detalle: string;
+    direccion: string;
+    empresa: string;
+    horas: number;
+    espacios: number;
+}
