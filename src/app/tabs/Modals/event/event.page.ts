bffce71582ca66ec5e6edd19e08e06900b475c08
ludamuac <luisa@cetys.edu.mx>
2020-05-18 01:14:00 -0700

    creacion de funcionaldiad para eventos detail

--- src/app/tabs/Modals/event/event.page.ts
+++ src/app/tabs/Modals/event/event.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController, NavParams, NavController } from '@ionic/angular';
+import { EventosService } from 'src/app/services/eventos.service';
+import { Evento } from 'src/models/evento.model';
 
 @Component({
   selector: 'app-event',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventPage implements OnInit {
 
-  constructor() { }
+  evento: Evento;
+
+  constructor(public modalCtrl: ModalController,
+              private navParams: NavParams,
+              private eventoService: EventosService,
+              private navCtrl: NavController) { }
 
   ngOnInit() {
+    const id = this.navParams.get('eventoId');
+    if (id) {
+      this.getEvento(id);
+    } else {
+      this.navCtrl.navigateRoot('tabs/home');
+    }
   }
+
+  dismiss() {
+    console.log(' Entro al dismiss ');
+    this.modalCtrl.dismiss();
+  }
+
+  getEvento(eventId: string) {
+    this.eventoService.getEvento(eventId).subscribe((evento: Evento) => {
+      this.evento = evento;
+    })
+  }
+
 
 }
